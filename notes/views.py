fae4ff9c215c85b8aa0696c336013587a7e8ef2e
John Jacob Ruiz <johnjacobdimaya0@gmail.com>
2025-04-21 16:24:51 +0800

    notes lists

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_lists.html', { 'notes': all_notes })
