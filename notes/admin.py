00015a4cbde5a4443a9f65ba50fc153302cae2e8
John Jacob Ruiz <johnjacobdimaya0@gmail.com>
2025-04-21 12:28:59 +0800

    add likes

--- notes/admin.py
+++ notes/admin.py
@@ -4,5 +4,8 @@ from . import models
 # Register your models here.
 @admin.register(models.Notes)
 class NotesAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'title', 'created_at')
+    list_display = ('__str__', 'title', 'likes', 'created_at')
+
+
+
 
--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Notes(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
+    likes = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Note {self.pk}"
+    
+    class Meta:
+        verbose_name_plural = 'notes'
