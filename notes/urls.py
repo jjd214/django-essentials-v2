b54e4d3c4723ca7bbf8575a401626b1303daa1a2
John Jacob Ruiz <johnjacobdimaya0@gmail.com>
2025-04-24 11:09:02 +0800

    complex class base view

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
-    path('notes/<int:id>', views.detail),
+    # path('notes', views.list),
+    # path('notes/<int:id>', views.detail),
+    # DetailView expeect pk not id
+    path('notes/<int:pk>', views.DetailView.as_view()),
+    path('notes', views.ListView.as_view())
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+# TemplateView and ListView, DetailView is different
+# ListView already make the query
+from django.views.generic import ListView, DetailView
+
+class ListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_lists.html"
+
+class DetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_details.html"
 
 # Create your views here.
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_lists.html', { 'notes': all_notes })
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_lists.html', { 'notes': all_notes })
 
-def detail(request, id):
-    try:
-        note = Notes.objects.get(pk=id)
-    except Notes.DoesNotExist:
-        raise Http404("Note id does not exist.")
-    return render(request, 'notes/notes_details.html', { 'note': note })
+# def detail(request, id):
+#     try:
+#         note = Notes.objects.get(pk=id)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note id does not exist.")
+#     return render(request, 'notes/notes_details.html', { 'note': note })
