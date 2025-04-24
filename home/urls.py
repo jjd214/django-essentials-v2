3c1bb9fb32801d2bd3193cb11e24fb1d88e58b2f
John Jacob Ruiz <johnjacobdimaya0@gmail.com>
2025-04-24 10:30:53 +0800

    implements class-based views

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('authorize', views.authorize),
+    # Class base view
+    path('', views.HomeView.as_view()),
+    # path('', views.home),
+    
+    path('authorize', views.AuthorizeView.as_view()),
+    # path('authorize', views.authorize),
 ]
--- home/views.py
+++ home/views.py
@@ -3,10 +3,23 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def home(request):
-    return render(request, 'home/welcome.html', { 'date': datetime.now })
+# Class base view
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-@login_required(login_url='/admin')
-def authorize(request):
-    return render(request, 'home/authorize.html', {})
+class HomeView(TemplateView):
+    template_name = "home/welcome.html"
+    extra_context = { 'date': datetime.now() }
+
+class AuthorizeView(LoginRequiredMixin, TemplateView):
+    template_name = "home/authorize.html"
+    # redirect
+    login_url = "/admin"
+
+# Create your views here.
+# def home(request):
+#     return render(request, 'home/welcome.html', { 'date': datetime.now })
+
+# @login_required(login_url='/admin')
+# def authorize(request):
+#     return render(request, 'home/authorize.html', {})
